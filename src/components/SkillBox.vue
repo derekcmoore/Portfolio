d89ad3951d0e488e73204f537322a66f7f81c193
Derek Moore <moorder1112@gmail.com>
2021-12-04 08:31:43 -0600

    Added skills section

---
+++ src/components/SkillBox.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="skill">
+    <img :src="require(`@/assets/img/${src}`)" alt="" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Nav",
+  props: {
+    src: String,
+  },
+  computed: {},
+  components: {},
+  methods: {},
+};
+</script>
+
+<style scoped lang="scss">
+.skill {
+  position: relative;
+  display: flex;
+  padding: 20px 60px;
+
+  @media (max-width: $responsive-width) {
+    padding: 20px 30px;
+  }
+
+  img {
+    position: relative;
+    max-width: 150px;
+    max-height: 60px;
+    opacity: 0.3;
+    filter: gray;
+    -webkit-filter: grayscale(0.5);
+    -webkit-filter: grayscale(50%);
+    filter: grayscale(50%);
+    cursor: pointer;
+
+    @media (max-width: $responsive-width) {
+      max-width: 100px;
+      max-height: 40px;
+    }
+
+    &:hover {
+      opacity: 1;
+      filter: none;
+      -webkit-filter: grayscale(0);
+    }
+  }
+}
+</style>
---
+++ src/views/sections/Skills.vue
@@ -0,0 +1,61 @@
+<template>
+  <section class="section static">
+    <div class="section-wrapper">
+      <div class="skill-panel">
+        <h4>Languages &amp; Skills</h4>
+        <div class="skill-table">
+          <SkillBox :src="'ruby-on-rails.svg'" />
+          <SkillBox :src="'vue.svg'" />
+          <SkillBox :src="'angular.svg'" />
+          <SkillBox :src="'react.svg'" />
+          <SkillBox :src="'python.svg'" />
+          <SkillBox :src="'aws.svg'" />
+          <SkillBox :src="'cPlus.svg'" />
+          <SkillBox :src="'cSharp.svg'" />
+          <SkillBox :src="'dot-net-core.svg'" />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import SkillBox from "../../components/SkillBox.vue";
+
+export default {
+  name: "Skills",
+  computed: {},
+  data() {
+    return {};
+  },
+  components: { SkillBox },
+  created() {},
+  methods: {},
+};
+</script>
+
+<style scoped lang="scss">
+.skill-panel {
+  position: relative;
+  top: -80px;
+  z-index: 9997;
+
+  box-shadow: 0 12px 30px 0 rgb(0 0 0 / 15%);
+  padding: 32px 10px;
+  text-align: center;
+  border-radius: 20px;
+  background-color: #ffffff;
+
+  h4 {
+    font-size: 1.3rem;
+    color: #9faaaf;
+  }
+}
+
+.skill-table {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  width: 100%;
+}
+</style>
