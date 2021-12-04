630d7e35cbb8305c0321079fe12d24dbb0a7ab7a
Derek Moore <moorder1112@gmail.com>
2021-12-04 08:45:48 -0600

    added experience and project templates

---
+++ src/views/sections/Experience.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="section static">
+    <div class="experience section-wrapper">
+      <h2 class="header">Experience</h2>
+      coming soon
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Experience",
+  computed: {},
+  components: {},
+  methods: {},
+};
+</script>
+
+<style scoped lang="scss">
+.section {
+  background-image: linear-gradient(
+    177deg,
+    rgba(172, 11, 137, 0.2) 0,
+    transparent 65%
+  );
+  padding-top: 65px;
+  padding-bottom: 80px;
+}
+
+.experience {
+  width: 100%;
+  height: 100vh;
+
+  .header {
+    color: #ba4cdb;
+
+    &::before {
+      border-top: 4px solid #ba4cdb;
+    }
+  }
+}
+</style>
---
+++ src/views/sections/Projects.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="section static">
+    <div class="projects section-wrapper">
+      <h2 class="header">Projects</h2>
+      <!-- <table class="grid">
+        <tr>
+          <td><h2>Test Project</h2></td>
+          <td>test</td>
+          <td>test</td>
+        </tr>
+      </table> -->
+      coming soon
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Projects",
+  computed: {},
+  components: {},
+  methods: {},
+};
+</script>
+
+<style scoped lang="scss">
+.section {
+  padding-top: 65px;
+}
+
+.projects {
+  width: 100%;
+  height: 100vh;
+
+  .header {
+    color: #4c96db;
+
+    &::before {
+      border-top: 4px solid #4c96db;
+    }
+  }
+
+  .grid {
+    max-width: 1280px;
+    margin-left: auto;
+    margin-right: auto;
+    width: 100%;
+
+    td {
+      width: calc(100% / 3);
+    }
+  }
+}
+</style>
