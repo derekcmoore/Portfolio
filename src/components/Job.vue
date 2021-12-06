84d68328df6200ac1f452a9ebdc4a686cbea9669
Derek Moore <moorder1112@gmail.com>
2021-12-06 13:25:25 -0600

    Added jobs to experience

---
+++ src/components/Job.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="job">
+    <div class="title">
+      <img :src="require(`@/assets/img/companies/${src}`)" alt="" />
+      <div class="timeline">
+        <div v-for="(job, index) in timeline" :key="index">
+          {{ job }}
+        </div>
+      </div>
+    </div>
+
+    <div class="content">
+      <ul>
+        <li v-for="(line, index) in content" :key="index">{{ line }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Job",
+  props: {
+    src: String,
+    timeline: Array,
+    content: Array,
+  },
+  computed: {},
+  components: {},
+  methods: {},
+};
+</script>
+
+<style scoped lang="scss">
+.job {
+  position: relative;
+  box-shadow: 0 12px 30px 0 rgb(0 0 0 / 15%);
+  margin-top: 100px;
+  padding: 32px 15px;
+  text-align: center;
+  border-radius: 20px;
+  background-color: #ffffff;
+
+  .title {
+    margin-bottom: 40px;
+    img {
+      max-width: 12em;
+      max-height: 5em;
+      @media (max-width: $responsive-width) {
+        width: 100%;
+      }
+    }
+
+    .timeline {
+      margin-top: 15px;
+      color: #9faaaf;
+    }
+  }
+
+  .content {
+    text-align: left;
+
+    ul {
+      list-style: none;
+      margin: 2em;
+      padding: 0;
+
+      li {
+        position: relative;
+        margin: 0;
+        margin-bottom: 1em;
+        padding-left: 1.5em;
+
+        &:after {
+          content: "";
+          height: 0.4em;
+          width: 0.4em;
+          background: #4cdb64;
+          display: block;
+          position: absolute;
+          transform: rotate(45deg);
+          top: 9px;
+          left: 0;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/sections/Experience.vue
+++ src/views/sections/Experience.vue
@@ -2,40 +2,71 @@
   <section class="section static">
     <div class="experience section-wrapper">
       <h2 class="header">Experience</h2>
-      coming soon
+      <Job
+        :src="'nelnet.png'"
+        :timeline="nelnetTimeline"
+        :content="nelnetContent"
+      />
+      <Job
+        :src="'dont_panic_labs.png'"
+        :timeline="dplTimeline"
+        :content="dplContent"
+      />
     </div>
   </section>
 </template>
 
 <script>
+import Job from "../../components/Job.vue";
+
 export default {
   name: "Experience",
   computed: {},
-  components: {},
+  data() {
+    return {
+      nelnetTimeline: [
+        "Software Engineer at PaymentSpring, June 2021-Present",
+        "Software Engineering Intern at PaymentSpring, May 2020-June 2021",
+      ],
+      nelnetContent: [
+        "Rewrote an internal admin tool using Ruby on Rails and Vue.js, increasing end-user productivity by reworking the UI and back-end functionality",
+        "Aided in the development of production and UAT environments with CloudFormation within AWS, using services such as Parameter Store, ECS, CodePipeline, Lambdas, etc.",
+        "Maintained microservices utilizing Ruby on Rails",
+        "Started and continue to work in a remote work environment, which enabled improvement of fluency in online team interaction",
+        "Led weekly meetings and discussions relating to progress and future product development of applications",
+        "Kept continuous communication with product users to ensure feedback became properly implemented during development",
+        "Implemented internationalization within multiple Rails applications",
+        "Rewrote the recurring payment scheduler to utilize AWS ECS scheduled tasks",
+      ],
+      dplTimeline: ["Software Engineering Intern, May 2019-August 2019"],
+      dplContent: [
+        "Developed a single solution using IDesign architecture principles",
+        "Created a project management tool from the beginning using Angular and ASP.NET Core",
+        "Wrote over 100 unit tests for API implementations using Moq",
+        "Implemented continuous integration and continuous deployment using Azure DevOps pipelines",
+        "Utilized Azure hosting for frontend and backend solutions",
+      ],
+    };
+  },
+  components: { Job },
   methods: {},
 };
 </script>
 
 <style scoped lang="scss">
 .section {
-  // background-image: linear-gradient(
-  //   177deg,
-  //   rgba(172, 11, 137, 0.2) 0,
-  //   transparent 65%
-  // );
   padding-top: 65px;
   padding-bottom: 80px;
 }
 
 .experience {
   width: 100%;
-  height: 100vh;
 
   .header {
-    color: #ba4cdb;
+    color: #4cdb64;
 
     &::before {
-      border-top: 4px solid #ba4cdb;
+      border-top: 4px solid #4cdb64;
     }
   }
 }
